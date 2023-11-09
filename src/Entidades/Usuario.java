c5cff79e343da06a9fc48ec050b72d3fc38175b0
Isabel <isabelbuenotab@gmail.com>
2023-11-09 12:25:20 +0100

    Añadir métodos

--- src/Entidades/Usuario.java
+++ src/Entidades/Usuario.java
@@ -3,7 +3,93 @@ package Entidades;
 import java.time.LocalDateTime;
 
 public class Usuario {
+	
+	//---------------------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------------------
+	
+	//Variables de instancia
 	private Long id;
 	private String nombreUsuario;
-	private LocalDateTime fechaUltimaConexion;
+	private LocalDateTime fechaUltimaConexion = LocalDateTime.now(); //Fecha más reciente = Última conexion
+	
+	//---------------------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------------------
+	
+	//Constructores
+	public Usuario(Long id, String nombreUsuario, LocalDateTime fechaUltimaConexion) {
+		//Sutituir this por setters para mayor eficiencia
+		setId(id);
+		setNombreUsuario(nombreUsuario);
+		setFechaUltimaConexion(fechaUltimaConexion);
+	}
+	
+	//Sobrecarga
+	//Permite valores por defecto
+	//Van llamando sucesivamente al constructor principal
+	
+	//Permite pasar el id como null por defecto
+	public Usuario(String nombreUsuario, LocalDateTime fechaUltimaConexion) {
+		this(null, nombreUsuario, fechaUltimaConexion);
+	}
+	
+	//Permite que solo sea necesario pasar el nombre del usuario
+	public Usuario(String nombreUsuario) {
+		this(null, nombreUsuario, LocalDateTime.now());
+	}
+	
+	//Permite que no sea obligatorio pasarle ningún valor y pone los mismos por defecto
+	public Usuario() {
+		this(null, "Anónimo", LocalDateTime.now());
+	}
+	
+	//---------------------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------------------
+
+	
+	//Getters 
+	public Long getId() {
+		return id;
+	}
+	
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	
+	public LocalDateTime getFechaUltimaConexion() {
+		return fechaUltimaConexion;
+	}
+	
+	
+	//Setters
+	public void setId(Long id) {
+		if(id !=null && id < 0){
+			throw new RuntimeException("El id no puede ser menor que 0 ");
+		}
+		this.id = id;
+	}
+	
+	public void setNombreUsuario(String nombreUsuario) {
+		if(nombreUsuario == null) {
+			throw new RuntimeException("El nombre no puede estar vacío");
+		}
+		this.nombreUsuario = nombreUsuario;
+	}
+	public void setFechaUltimaConexion(LocalDateTime fechaUltimaConexion) {
+		if(fechaUltimaConexion.isAfter(LocalDateTime.now()) || fechaUltimaConexion == null) {
+			throw new RuntimeException("La fecha no puede ser posterior a la fecha actual ni estar vacía");
+		}
+		this.fechaUltimaConexion = fechaUltimaConexion;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombreUsuario=" + nombreUsuario + ", fechaUltimaConexion=" + fechaUltimaConexion
+				+ "]";
+	}
+	
+	
+	//---------------------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------------------
+	
+	
 }
--- src/Pruebas/UsuarioPruebas.java
+++ src/Pruebas/UsuarioPruebas.java
@@ -1,8 +1,14 @@
 package Pruebas;
 
+import Entidades.Usuario;
+
 public class UsuarioPruebas {
 
 	public static void main(String[] args) {
+		Usuario usuario  = new Usuario();
+		
+		System.out.println(usuario);
+		
 		
 	}
 
