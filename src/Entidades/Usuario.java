da81de9e106c08d1885ded2f5828a4d215c52e59
Isabel <isabelbuenotab@gmail.com>
2023-11-09 11:52:00 +0100

    Paquetes Entidades y Pruebas creados Clases Usuario y UsuarioPruebas creadas

---
+++ src/Entidades/Usuario.java
@@ -0,0 +1,9 @@
+package Entidades;
+
+import java.time.LocalDateTime;
+
+public class Usuario {
+	private Long id;
+	private String nombreUsuario;
+	private LocalDateTime fechaUltimaConexion;
+}
---
+++ src/Pruebas/UsuarioPruebas.java
@@ -0,0 +1,9 @@
+package Pruebas;
+
+public class UsuarioPruebas {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
